67462c82b26aaf21bc37d2332c638e04abdbd250
Nate Allen <neight.allen@gmail.com>
2017-07-04 15:59:15 -0600

    has one working test

--- test/entries-test.js
+++ test/entries-test.js
@@ -4,6 +4,7 @@ var test      = require('selenium-webdriver/testing');
 
 test.describe('testing my simple blog', function() {
   var driver;
+  var host = "http://localhost:8080"
   this.timeout(10000);
 
   test.beforeEach(function() {
@@ -14,5 +15,14 @@ test.describe('testing my simple blog', function() {
 
   test.afterEach(function() {
     driver.quit();
+  })
+
+  test.it("lists all the entries on load", function() {
+    driver.get(`${host}`)
+    driver.findElement({id: "entries"})
+    .findElements({css: ".entry"})
+    .then(function (entries) {
+      assert.lengthOf(entries, 3);
+    })
   })
 });
