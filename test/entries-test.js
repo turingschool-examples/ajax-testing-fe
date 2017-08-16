438cb3aff0ac4ca12d7c9e9cf9f59492374dfdf6
laurenfazah <laurenfazah@gmail.com>
2017-08-16 15:45:53 -0600

    Refactor and restructure

--- test/entries-test.js
+++ test/entries-test.js
@@ -1,6 +1,6 @@
 var assert    = require('chai').assert;
 var webdriver = require('selenium-webdriver');
-var until = webdriver.until;
+var until     = webdriver.until;
 var test      = require('selenium-webdriver/testing');
 var frontEndLocation = "http://localhost:8080"
 
@@ -12,18 +12,18 @@ test.describe('testing my simple blog', function() {
     driver = new webdriver.Builder()
       .forBrowser('chrome')
       .build();
-  })
+  });
 
   test.afterEach(function() {
     driver.quit();
-  })
+  });
 
   test.it("lists all the entries on load", function() {
-    driver.get(`${frontEndLocation}`)
-    driver.wait(until.elementLocated({css: "#entries .entry"}))
+    driver.get(`${frontEndLocation}`);
+    driver.wait(until.elementLocated({css: "#entries .entry"}));
     driver.findElements({css: "#entries .entry"})
     .then(function (entries) {
       assert.lengthOf(entries, 100);
-    })
-  })
+    });
+  });
 });
